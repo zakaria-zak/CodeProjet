132c537ec2cab2424d2cd5b12010f7875a65df86
Yassabio <yassabio@hotmail.com>
2017-05-18 18:45:47 +0200

    kasiski en place

--- DecryptageVigenere.c
+++ DecryptageVigenere.c
@@ -1,18 +1,5 @@
 #include "DecryptageVigenere.h"
 
-int longueurChaine(const char* chaine)
-{
-    int nombreDeCaracteres = 0;
-    char caractereActuel = 0;
-    do
-    {
-        caractereActuel = chaine[nombreDeCaracteres];
-        nombreDeCaracteres++;
-    }
-    while(caractereActuel != '\0'); // On boucle tant qu'on n'est pas arrivé à l'\0
-    nombreDeCaracteres--; // On retire 1 caractère de long pour ne pas compter le caractère \0
-    return nombreDeCaracteres;
-}
 int Kasiski(ANALYSE freq, gchar* texteCrypte){
 
 	int i,j,kas,taillepgor;
