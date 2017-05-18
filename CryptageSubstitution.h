99e96f27c61b27658ee3a9fd0a61408daf83c489
Yassabio <yassabio@hotmail.com>
2017-05-18 13:17:21 +0200

    cryptage propre

--- CryptageSubstitution.h
+++ CryptageSubstitution.h
@@ -5,4 +5,7 @@
 
 #endif
 
-gchar* CryptageSubstitution(gchar* TexteClair);
+void CryptageSubstitution(gchar* TexteCrypte, gchar* TexteClair);
+void GenereCle(gchar T[]);
+int *tirage(int nombre, int min, int max);
+int Ascii0_25(int nbr);
