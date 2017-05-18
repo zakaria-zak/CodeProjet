c97edf2e9b45357655a0e7996bba3114a9aa4683
younesbens <younes.benyamna@gmail.com>
2017-05-18 13:21:54 +0200

    younes cryptage

--- CryptageVigenere.c
+++ CryptageVigenere.c
@@ -1,6 +1,17 @@
 #include "CryptageVigenere.h"
 
-gchar* CryptageVigenere(gchar* TexteClair, gchar* Cle)
+void CryptageVigenere(char* resultat,char* texteClair,char* Cle)
 {
-	return TexteClair;
+   int i;
+   char a,b,c;
+   int length = strlen(Cle);
+		for ( i = 0; i < strlen(texteClair); i++)
+		{
+			a= texteClair[i] - 97;
+			b= Cle[i%length] - 97;
+			c = (a+b) ;
+			if(c > 25)
+				c=c-26;
+			resultat[i] = c + 97;
+		}
 }
